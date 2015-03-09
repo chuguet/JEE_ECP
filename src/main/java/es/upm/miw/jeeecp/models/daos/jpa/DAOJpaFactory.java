c6753657bf9fb66fc9bf4c7436db37d45446f331
Huguet <huguet10@gmail.com>
2015-03-09 21:15:18 +0100

    Se cambia el nombre de la unidad de persistencia

--- src/main/java/es/upm/miw/jeeecp/models/daos/jpa/DAOJpaFactory.java
+++ src/main/java/es/upm/miw/jeeecp/models/daos/jpa/DAOJpaFactory.java
@@ -10,28 +10,27 @@ import es.upm.miw.jeeecp.models.daos.TemaDAO;
 import es.upm.miw.jeeecp.models.daos.VotoDAO;
 
 public class DAOJpaFactory extends DAOFactory {
-	private static final String PERSISTENCE_UNIT = "tictactoe";
+    private static final String PERSISTENCE_UNIT = "jee_ecp";
 
-	private static EntityManagerFactory entityManagerFactory = Persistence
-			.createEntityManagerFactory(PERSISTENCE_UNIT);
+    private static EntityManagerFactory entityManagerFactory = Persistence
+            .createEntityManagerFactory(PERSISTENCE_UNIT);
 
-	public DAOJpaFactory() {
-		LogManager.getLogger(DAOJpaFactory.class).debug(
-				"create Entity Manager Factory");
-	}
+    public DAOJpaFactory() {
+        LogManager.getLogger(DAOJpaFactory.class).debug("create Entity Manager Factory");
+    }
 
-	public static EntityManagerFactory getEntityManagerFactory() {
-		return entityManagerFactory;
-	}
+    public static EntityManagerFactory getEntityManagerFactory() {
+        return entityManagerFactory;
+    }
 
-	@Override
-	public TemaDAO getTemaDAO() {
-		return new TemaDAOJpa();
-	}
+    @Override
+    public TemaDAO getTemaDAO() {
+        return new TemaDAOJpa();
+    }
 
-	@Override
-	public VotoDAO getVotoDAO() {
-		return new VotoDAOJpa();
-	}
+    @Override
+    public VotoDAO getVotoDAO() {
+        return new VotoDAOJpa();
+    }
 
 }
