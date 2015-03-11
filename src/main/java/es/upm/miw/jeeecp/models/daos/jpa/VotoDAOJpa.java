53e075bff3e46446d95408dae1f4350d9d90508a
Huguet <huguet10@gmail.com>
2015-03-12 00:14:17 +0100

    Se añade un test y se continua con el join. Aun falta para dejarlo fino. Lo dejo por hoy

--- src/main/java/es/upm/miw/jeeecp/models/daos/jpa/VotoDAOJpa.java
+++ src/main/java/es/upm/miw/jeeecp/models/daos/jpa/VotoDAOJpa.java
@@ -8,7 +8,10 @@ import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
+import org.eclipse.persistence.jpa.JpaQuery;
+
 import es.upm.miw.jeeecp.models.daos.VotoDAO;
+import es.upm.miw.jeeecp.models.entities.TemaEntity;
 import es.upm.miw.jeeecp.models.entities.VotoEntity;
 import es.upm.miw.jeeecp.models.utils.NivelEstudios;
 
@@ -25,15 +28,19 @@ public class VotoDAOJpa extends GenericDAOJpa<VotoEntity, Integer> implements Vo
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Double> query = builder.createQuery(Double.class);
         Root<VotoEntity> root = query.from(VotoEntity.class);
+        Root<TemaEntity> rootTema = query.from(TemaEntity.class);
+        rootTema.join("votos");
         Expression<Number> exp = root.get("valoracion");
-        query.multiselect(builder.avg(exp));
-        Predicate predicate = builder.equal(root.get("nivelEstudios").as(NivelEstudios.class),
-                nivelEstudios);
+        query.select(builder.avg(exp));
+        Predicate predicate = builder.and(
+                builder.equal(root.get("nivelEstudios").as(NivelEstudios.class), nivelEstudios),
+                builder.equal(rootTema.get("id"), idTema));
         query.where(predicate);
 
         TypedQuery<Double> typedQuery = entityManager.createQuery(query);
         Double result = typedQuery.getSingleResult();
         entityManager.close();
+        System.out.println(typedQuery.unwrap(JpaQuery.class).getDatabaseQuery().getSQLString());
         return result;
     }
 }
--- src/test/java/es/upm/miw/jeeecp/models/daos/jpa/VotoDAOJpaTest.java
+++ src/test/java/es/upm/miw/jeeecp/models/daos/jpa/VotoDAOJpaTest.java
@@ -2,39 +2,93 @@ package es.upm.miw.jeeecp.models.daos.jpa;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
-import es.upm.miw.jeeecp.models.daos.DAOFactory;
+import es.upm.miw.jeeecp.models.daos.TemaDAO;
 import es.upm.miw.jeeecp.models.daos.VotoDAO;
+import es.upm.miw.jeeecp.models.daos.jpa.data.TemaDAOJpaTestData;
+import es.upm.miw.jeeecp.models.entities.TemaEntity;
 import es.upm.miw.jeeecp.models.entities.VotoEntity;
 import es.upm.miw.jeeecp.models.utils.NivelEstudios;
 
 public class VotoDAOJpaTest {
 
-    @Test
+    private VotoDAO daoVoto = DAOJpaFactory.getFactory().getVotoDAO();
+
+    private TemaDAO daoTema = DAOJpaFactory.getFactory().getTemaDAO();
+
+    private TemaDAOJpaTestData data;
+
+    @Before
+    public void inti() {
+        data = new TemaDAOJpaTestData();
+    }
+
+    // @Test
     public void createVotoTest() {
         VotoEntity voto;
-        DAOFactory daoFactory = DAOFactory.getFactory();
-        VotoDAO votoDAO = daoFactory.getVotoDAO();
 
         voto = new VotoEntity(3, "192.168.1.1", NivelEstudios.BACHILLERATO);
-        votoDAO.create(voto);
+        daoVoto.create(voto);
         voto = new VotoEntity(4, "192.168.1.1", NivelEstudios.BACHILLERATO);
-        votoDAO.create(voto);
+        daoVoto.create(voto);
         voto = new VotoEntity(2, "192.168.1.1", NivelEstudios.LICENCIADO);
-        votoDAO.create(voto);
+        daoVoto.create(voto);
         voto = new VotoEntity(5, "192.168.1.1", NivelEstudios.DIPLOMADO);
-        votoDAO.create(voto);
+        daoVoto.create(voto);
         voto = new VotoEntity(9, "192.168.1.1", NivelEstudios.DIPLOMADO);
-        votoDAO.create(voto);
+        daoVoto.create(voto);
         voto = new VotoEntity(7, "192.168.1.1", NivelEstudios.DIPLOMADO);
-        votoDAO.create(voto);
+        daoVoto.create(voto);
 
-        assertEquals(new Double(3.5), votoDAO.recuperarMediaVotacionesPorNivelDeEstudiosYTema(1,
+        assertEquals(new Double(3.5), daoVoto.recuperarMediaVotacionesPorNivelDeEstudiosYTema(1,
                 NivelEstudios.BACHILLERATO));
         assertEquals(new Double(7),
-                votoDAO.recuperarMediaVotacionesPorNivelDeEstudiosYTema(1, NivelEstudios.DIPLOMADO));
-        assertEquals(new Double(2), votoDAO.recuperarMediaVotacionesPorNivelDeEstudiosYTema(1,
+                daoVoto.recuperarMediaVotacionesPorNivelDeEstudiosYTema(1, NivelEstudios.DIPLOMADO));
+        assertEquals(new Double(2), daoVoto.recuperarMediaVotacionesPorNivelDeEstudiosYTema(1,
                 NivelEstudios.LICENCIADO));
+    }
+
+    @Test
+    public void testCreate() {
+        List<TemaEntity> temasData = this.saveData();
+        for (TemaEntity tema : temasData) {
+            assertEquals(this.calculateMedia(tema.getVotos(), NivelEstudios.BACHILLERATO),
+                    daoVoto.recuperarMediaVotacionesPorNivelDeEstudiosYTema(tema.getId(),
+                            NivelEstudios.BACHILLERATO));
+        }
+    }
+
+    private Double calculateMedia(List<VotoEntity> votos, NivelEstudios nivelEstudios) {
+        Double result = 0d;
+        Integer total = 0;
+        for (VotoEntity voto : votos) {
+            if (nivelEstudios.equals(voto.getNivelEstudios())) {
+                result += voto.getValoracion();
+                total++;
+            }
+        }
+        if (total == 0) {
+            return null;
+        }
+        return result / total;
+    }
+
+    private List<TemaEntity> saveData() {
+        List<TemaEntity> temasData = new ArrayList<TemaEntity>();
+        while (data.hasNextTemas()) {
+            if (data.getTema() != null) {
+                daoTema.create(data.getTema());
+                temasData.add(data.getTema());
+            }
+            data.nextTema();
+        }
+        daoTema.create(data.getTema());
+        temasData.add(data.getTema());
+        return temasData;
     }
 }
