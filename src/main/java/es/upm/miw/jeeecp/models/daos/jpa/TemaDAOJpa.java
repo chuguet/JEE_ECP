d117fee4e9bc90057a9a6e2d712578f229281fec
Huguet <huguet10@gmail.com>
2015-03-11 21:16:50 +0100

    Se añade con criteria contar el numero de votos

--- src/main/java/es/upm/miw/jeeecp/models/daos/jpa/TemaDAOJpa.java
+++ src/main/java/es/upm/miw/jeeecp/models/daos/jpa/TemaDAOJpa.java
@@ -1,5 +1,12 @@
 package es.upm.miw.jeeecp.models.daos.jpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 import es.upm.miw.jeeecp.models.daos.TemaDAO;
 import es.upm.miw.jeeecp.models.entities.TemaEntity;
 
@@ -7,6 +14,21 @@ public class TemaDAOJpa extends GenericDAOJpa<TemaEntity, Integer> implements Te
 
     public TemaDAOJpa() {
         super(TemaEntity.class);
+    }
+
+    @Override
+    public Long countVotos(Integer id) {
+        EntityManager entityManager = DAOJpaFactory.getEntityManagerFactory().createEntityManager();
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Long> query = builder.createQuery(Long.class);
+        Root<TemaEntity> root = query.from(TemaEntity.class);
+        query.select(builder.count(root.get("votos")));
+        Predicate predicate = builder.equal(root.get("id").as(Integer.class), id);
+        query.where(predicate);
+        TypedQuery<Long> typedQuery = entityManager.createQuery(query);
+        Long result = typedQuery.getSingleResult();
+        entityManager.close();
+        return result;
     }
 
 }
