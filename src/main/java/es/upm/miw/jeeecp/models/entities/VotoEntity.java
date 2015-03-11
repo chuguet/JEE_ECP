a20a53efce89a711686b5a41dcefacb6958f1e30
Huguet <huguet10@gmail.com>
2015-03-11 19:42:19 +0100

    Se quitan validaciones

--- src/main/java/es/upm/miw/jeeecp/models/entities/VotoEntity.java
+++ src/main/java/es/upm/miw/jeeecp/models/entities/VotoEntity.java
@@ -6,26 +6,18 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.Pattern;
 
 import es.upm.miw.jeeecp.models.utils.NivelEstudios;
 
 @Entity
 public class VotoEntity {
 
-    private static final String PATTERN_IP_ADDRESS = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Max(10)
-    @Min(0)
     private Integer valoracion;
 
-    @Pattern(regexp = PATTERN_IP_ADDRESS)
     private String ip;
 
     @Enumerated(EnumType.STRING)
