f34e32ff8654c3e5a784abe4e607dab6f30e8551
Huguet <huguet10@gmail.com>
2015-03-12 21:04:29 +0100

    Se prepara la factoria de jpa para soportar tests

--- src/main/java/es/upm/miw/jeeecp/models/daos/jpa/DAOJpaFactory.java
+++ src/main/java/es/upm/miw/jeeecp/models/daos/jpa/DAOJpaFactory.java
@@ -1,9 +1,13 @@
 package es.upm.miw.jeeecp.models.daos.jpa;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import org.apache.logging.log4j.LogManager;
+import org.eclipse.persistence.config.PersistenceUnitProperties;
 
 import es.upm.miw.jeeecp.models.daos.DAOFactory;
 import es.upm.miw.jeeecp.models.daos.TemaDAO;
@@ -12,15 +16,26 @@ import es.upm.miw.jeeecp.models.daos.VotoDAO;
 public class DAOJpaFactory extends DAOFactory {
     private static final String PERSISTENCE_UNIT = "jee_ecp";
 
-    private static EntityManagerFactory entityManagerFactory = Persistence
-            .createEntityManagerFactory(PERSISTENCE_UNIT);
+    private static EntityManagerFactory entityManagerFactory;
 
     public DAOJpaFactory() {
         LogManager.getLogger(DAOJpaFactory.class).debug("create Entity Manager Factory");
     }
 
     public static EntityManagerFactory getEntityManagerFactory() {
+        if (entityManagerFactory == null) {
+            entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+            LogManager.getLogger(DAOJpaFactory.class).debug("create Entity Manager Factory");
+        }
         return entityManagerFactory;
+    }
+
+    public static void dropAndCreateTables() {
+        Map<String, String> properties = new HashMap<>();
+        properties.put(PersistenceUnitProperties.DDL_GENERATION,
+                PersistenceUnitProperties.DROP_AND_CREATE);
+        entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT, properties);
+        LogManager.getLogger("Vuelvo las tablas");
     }
 
     @Override
