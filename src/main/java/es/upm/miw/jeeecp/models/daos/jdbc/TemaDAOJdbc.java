4b475500756f1169ee06bd1bef934242c422727a
Huguet <huguet10@gmail.com>
2015-03-12 21:58:20 +0100

    Se añaden los metodos de jdbc

--- src/main/java/es/upm/miw/jeeecp/models/daos/jdbc/TemaDAOJdbc.java
+++ src/main/java/es/upm/miw/jeeecp/models/daos/jdbc/TemaDAOJdbc.java
@@ -1,19 +1,25 @@
 package es.upm.miw.jeeecp.models.daos.jdbc;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import es.upm.miw.jeeecp.models.daos.TemaDAO;
 import es.upm.miw.jeeecp.models.entities.TemaEntity;
 
 public class TemaDAOJdbc extends GenericDAOJdbc<TemaEntity, Integer> implements TemaDAO {
 
-    @Override
-    public Long countVotos(Integer idTema) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+    private static final Logger LOG = LogManager.getLogger(TemaDAOJdbc.class);
 
     private static final String SQL_CREATE_TABLE = "CREATE TABLE %s (%s INT NOT NULL, %s VARCHAR(255), %s VARCHAR(255), PRIMARY KEY (%s))";
+
+    private static final String INSERT_QUERY = "INSERT INTO %s (%s,%s,%s) VALUES (%d,'%s','%s')";
+
+    private static final String QUERY_UPDATE = "UPDATE %s SET %s='%s', %s='%s' WHERE ID=%d";
 
     public static String sqlToCreateTable() {
         return String.format(SQL_CREATE_TABLE, TemaEntity.TABLE, TemaEntity.ID, TemaEntity.NOMBRE,
@@ -22,31 +28,66 @@ public class TemaDAOJdbc extends GenericDAOJdbc<TemaEntity, Integer> implements 
 
     @Override
     public void create(TemaEntity temaEntity) {
-        // TODO Auto-generated method stub
+        this.updateSql(String.format(INSERT_QUERY, TemaEntity.TABLE, TemaEntity.ID,
+                TemaEntity.NOMBRE, TemaEntity.PREGUNTA, temaEntity.getId(), temaEntity.getNombre(),
+                temaEntity.getPregunta()));
     }
 
     @Override
     public TemaEntity read(Integer id) {
-        // TODO Auto-generated method stub
-        return null;
+        ResultSet resultSet = this.query(String.format(SQL_SELECT_ID, TemaEntity.TABLE, id));
+        return this.mapperResultSet(resultSet);
     }
 
     @Override
-    public void update(TemaEntity entity) {
-        // TODO Auto-generated method stub
-
+    public void update(TemaEntity temaEntity) {
+        this.updateSql(String.format(QUERY_UPDATE, TemaEntity.TABLE, TemaEntity.ID,
+                TemaEntity.NOMBRE, TemaEntity.PREGUNTA, temaEntity.getId(), temaEntity.getNombre(),
+                temaEntity.getPregunta()));
     }
 
     @Override
     public void deleteById(Integer id) {
-        // TODO Auto-generated method stub
-
+        this.updateSql(String.format(SQL_DELETE_ID, TemaEntity.TABLE, id));
     }
 
     @Override
     public List<TemaEntity> findAll() {
-        // TODO Auto-generated method stub
-        return null;
+        ResultSet resultSet = this.query(String.format(SQL_SELECT_ALL, TemaEntity.TABLE));
+        return this.mapperListResultSet(resultSet);
+    }
+
+    @Override
+    public Long countVotos(Integer idTema) {
+        return (long) this.read(idTema).getVotos().size();
+    }
+
+    private List<TemaEntity> mapperListResultSet(ResultSet resultSet) {
+        List<TemaEntity> result = new ArrayList<TemaEntity>();
+        try {
+            while (resultSet != null && resultSet.next()) {
+                result.add(new TemaEntity(resultSet.getInt(TemaEntity.ID), resultSet
+                        .getString(TemaEntity.NOMBRE), resultSet.getString(TemaEntity.PREGUNTA),
+                        null));
+            }
+        } catch (SQLException e) {
+            LOG.error(e.getMessage());
+        }
+        return result;
+    }
+
+    private TemaEntity mapperResultSet(ResultSet resultSet) {
+        TemaEntity result = null;
+        try {
+            if (resultSet != null && resultSet.next()) {
+                result = new TemaEntity(resultSet.getInt(TemaEntity.ID),
+                        resultSet.getString(TemaEntity.NOMBRE),
+                        resultSet.getString(TemaEntity.PREGUNTA), null);
+            }
+        } catch (SQLException e) {
+            LOG.error(e.getMessage());
+        }
+        return result;
     }
 
 }
