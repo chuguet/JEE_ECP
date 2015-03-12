89d6481f177e321eae15d5193fca24aeb4543ff1
Huguet <huguet10@gmail.com>
2015-03-12 21:00:36 +0100

    Me siento y arreglo el antipatron con un assert para obligar a que inyecte la factoria

--- src/main/java/es/upm/miw/jeeecp/models/daos/DAOFactory.java
+++ src/main/java/es/upm/miw/jeeecp/models/daos/DAOFactory.java
@@ -1,25 +1,19 @@
 package es.upm.miw.jeeecp.models.daos;
 
-import es.upm.miw.jeeecp.models.daos.jpa.DAOJpaFactory;
-
 public abstract class DAOFactory {
-	public static DAOFactory factory = null;
+    public static DAOFactory factory = null;
 
-	public static void setFactory(DAOFactory factory) {
-		DAOFactory.factory = factory;
-	}
+    public static void setFactory(DAOFactory factory) {
+        DAOFactory.factory = factory;
+    }
 
-	// TODO romper esta referencia a DaoJpaFactory pq crea una dependencia
-	// bidireccional
-	public static DAOFactory getFactory() {
-		if (factory == null) {
-			factory = new DAOJpaFactory();
-		}
-		return factory;
-	}
+    public static DAOFactory getFactory() {
+        assert (factory != null);
+        return factory;
+    }
 
-	public abstract TemaDAO getTemaDAO();
+    public abstract TemaDAO getTemaDAO();
 
-	public abstract VotoDAO getVotoDAO();
+    public abstract VotoDAO getVotoDAO();
 
 }
