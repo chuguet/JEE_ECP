b062d5a699ddf7d2102a3ccde3b0fb6e9f9a1a9f
Huguet <huguet10@gmail.com>
2015-03-14 15:13:31 +0100

    Despues de procesar se le dice al bean que devuelva un objeto a la jsp nuevo

--- src/main/java/es/upm/miw/jeeecp/views/web/beans/AnadirTemaBean.java
+++ src/main/java/es/upm/miw/jeeecp/views/web/beans/AnadirTemaBean.java
@@ -5,26 +5,27 @@ import es.upm.miw.jeeecp.models.entities.TemaEntity;
 
 public class AnadirTemaBean extends ViewBean {
 
-	private TemaEntity tema;
+    private TemaEntity tema;
 
-	public AnadirTemaBean() {
-	}
+    public AnadirTemaBean() {
+    }
 
-	public AnadirTemaBean(TemaEntity tema) {
-		this.tema = tema;
-	}
+    public AnadirTemaBean(TemaEntity tema) {
+        this.tema = tema;
+    }
 
-	public TemaEntity getTema() {
-		return tema;
-	}
+    public TemaEntity getTema() {
+        return tema;
+    }
 
-	public void setTema(TemaEntity tema) {
-		this.tema = tema;
-	}
+    public void setTema(TemaEntity tema) {
+        this.tema = tema;
+    }
 
-	public void process(){
-		AnadirTemaController anadirTemaController = this.getControllerFactory().getAnadirTemaController();
-		anadirTemaController.anadirTema(this.tema);
-	}
-
+    public void process() {
+        AnadirTemaController anadirTemaController = this.getControllerFactory()
+                .getAnadirTemaController();
+        anadirTemaController.anadirTema(this.getTema());
+        this.setTema(new TemaEntity());
+    }
 }
