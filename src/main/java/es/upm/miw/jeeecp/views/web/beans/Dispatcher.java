4db69cec8295982b470161184803c0693e18edaa
bl0812 <bl0812@192.168.7.24>
2015-03-14 12:02:22 +0100

    Se introduce una aclaracion de clase

--- src/main/java/es/upm/miw/jeeecp/views/web/beans/Dispatcher.java
+++ src/main/java/es/upm/miw/jeeecp/views/web/beans/Dispatcher.java
@@ -13,80 +13,101 @@ public class Dispatcher extends HttpServlet {
 
 	private static final long serialVersionUID = -8237077722948003615L;
 
-    private static String PATH_ROOT_VIEW = "/jsp/";
-	
+	private static String PATH_ROOT_VIEW = "/jsp/";
+
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-        String action = request.getPathInfo().substring(1);
+		String action = request.getPathInfo().substring(1);
 
-        String view;
-        switch (action) {
-        case "votar":
-            /*PersonaView personaView = new PersonaView();
-            personaView.setPersona(new Persona());
-            request.setAttribute(action, personaView);*/
-            view = action;
-            break;
-        case "anadirtema":
-            /*RolView rolView = new RolView();
-            request.setAttribute(action, rolView);*/
-            view = action;
-            break;
-        case "eliminartema":
-            /*RolView rolView = new RolView();
-            request.setAttribute(action, rolView);*/
-            view = action;
-            break;
-        case "vervotaciones":
-            /*RolView rolView = new RolView();
-            request.setAttribute(action, rolView);*/
-            view = action;
-            break;
-        default:
-            view = "home";
-        }
+		String view;
+		switch (action) {
+		case "votar":
+			/*
+			 * PersonaView personaView = new PersonaView();
+			 * personaView.setPersona(new Persona());
+			 * request.setAttribute(action, personaView);
+			 */
+			view = action;
+			break;
+		case "anadirtema":
+			/*
+			 * RolView rolView = new RolView(); request.setAttribute(action,
+			 * rolView);
+			 */
+			view = action;
+			break;
+		case "eliminartema":
+			/*
+			 * RolView rolView = new RolView(); request.setAttribute(action,
+			 * rolView);
+			 */
+			view = action;
+			break;
+		case "vervotaciones":
+			/*
+			 * RolView rolView = new RolView(); hecho en el @init el
+			 * controllerejb porque el managedbean no nfuncionaara cuando no
+			 * estemos en jsf y tendremos que inyectarlo desde aquí con un new
+			 * rolView.setControllerFactory(controllerEJB);
+			 * request.setAttribute(action, rolView);
+			 */
+			view = action;
+			break;
+		default:
+			view = "home";
+		}
 
-        this.getServletContext().getRequestDispatcher(PATH_ROOT_VIEW + view + ".jsp")
-                .forward(request, response);
+		this.getServletContext()
+				.getRequestDispatcher(PATH_ROOT_VIEW + view + ".jsp")
+				.forward(request, response);
 	}
-	
+
 	@Override
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-        String action = request.getPathInfo().substring(1);
+		String action = request.getPathInfo().substring(1);
 
-        String view;
-        switch (action) {
-        case "votar":
-            /*PersonaView personaView = new PersonaView();
-            personaView.setPersona(new Persona());
-            request.setAttribute(action, personaView);*/
-            view = action;
-            break;
-        case "anadirtema":
-        	System.out.println(request.getParameter("nombre"));
-        	System.out.println(request.getParameter("pregunta"));
-            /*RolView rolView = new RolView();
-            request.setAttribute(action, rolView);*/
-            view = action;
-            break;
-        case "eliminartema":
-            /*RolView rolView = new RolView();
-            request.setAttribute(action, rolView);*/
-            view = action;
-            break;
-        case "vervotaciones":
-            /*RolView rolView = new RolView();
-            request.setAttribute(action, rolView);*/
-            view = action;
-            break;
-        default:
-            view = "home";
-        }
+		String view;
+		switch (action) {
+		case "votar":
+			/*
+			 * PersonaView personaView = new PersonaView();
+			 * personaView.setPersona(new Persona());
+			 * request.setAttribute(action, personaView);
+			 */
+			view = action;
+			break;
+		case "anadirtema":
+			System.out.println(request.getParameter("nombre"));
+			System.out.println(request.getParameter("pregunta"));
+			/*
+			 * RolView rolView = new RolView(); request.setAttribute(action,
+			 * rolView);
+			 */
+			view = action;
+			break;
+		case "eliminartema":
+			/*
+			 * RolView rolView = new RolView(); request.setAttribute(action,
+			 * rolView);
+			 */
+			view = action;
+			break;
+		case "vervotaciones":
+			/*
+			 * RolView rolView = new RolView(); request.setAttribute(action,
+			 * rolView);
+			 */
+			view = action;
+			break;
+		default:
+			view = "home";
+		}
 
-        this.getServletContext().getRequestDispatcher(PATH_ROOT_VIEW + view + ".jsp")
-                .forward(request, response);
+		this.getServletContext()
+				.getRequestDispatcher(PATH_ROOT_VIEW + view + ".jsp")
+				.forward(request, response);
 	}
 
 }
