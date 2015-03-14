7e70f5f6da1a70c9e138f57520be189a91957ce5
Huguet <huguet10@gmail.com>
2015-03-14 21:56:59 +0100

    Se arregla i18n para que este en sesion y funcione para toda la aplicacion

--- src/main/java/es/upm/miw/jeeecp/views/web/beans/I18n.java
+++ src/main/java/es/upm/miw/jeeecp/views/web/beans/I18n.java
@@ -2,15 +2,22 @@ package es.upm.miw.jeeecp.views.web.beans;
 
 import java.util.Locale;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
 @ManagedBean
+@SessionScoped
 public class I18n {
 
     private Locale locale;
 
     public I18n() {
+    }
+
+    @PostConstruct
+    public void init() {
         this.setLocale(FacesContext.getCurrentInstance().getExternalContext().getRequestLocale());
     }
 
