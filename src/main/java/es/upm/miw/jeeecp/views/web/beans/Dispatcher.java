2bf811ebdf53c48bfc44ea70e414dd8374136679
bl0812 <bl0812@192.168.7.24>
2015-03-14 10:53:07 +0100

    Se monta un formulario para añadir tema y llegan los parametros al dispatcher

--- src/main/java/es/upm/miw/jeeecp/views/web/beans/Dispatcher.java
+++ src/main/java/es/upm/miw/jeeecp/views/web/beans/Dispatcher.java
@@ -50,5 +50,43 @@ public class Dispatcher extends HttpServlet {
         this.getServletContext().getRequestDispatcher(PATH_ROOT_VIEW + view + ".jsp")
                 .forward(request, response);
 	}
+	
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+        String action = request.getPathInfo().substring(1);
+
+        String view;
+        switch (action) {
+        case "votar":
+            /*PersonaView personaView = new PersonaView();
+            personaView.setPersona(new Persona());
+            request.setAttribute(action, personaView);*/
+            view = action;
+            break;
+        case "anadirtema":
+        	System.out.println(request.getParameter("nombre"));
+        	System.out.println(request.getParameter("pregunta"));
+            /*RolView rolView = new RolView();
+            request.setAttribute(action, rolView);*/
+            view = action;
+            break;
+        case "eliminartema":
+            /*RolView rolView = new RolView();
+            request.setAttribute(action, rolView);*/
+            view = action;
+            break;
+        case "vervotaciones":
+            /*RolView rolView = new RolView();
+            request.setAttribute(action, rolView);*/
+            view = action;
+            break;
+        default:
+            view = "home";
+        }
+
+        this.getServletContext().getRequestDispatcher(PATH_ROOT_VIEW + view + ".jsp")
+                .forward(request, response);
+	}
 
 }
--- src/main/webapp/jsp/anadirtema.jsp
+++ src/main/webapp/jsp/anadirtema.jsp
@@ -9,6 +9,16 @@
 <body>
 	<h2>A&ntilde;adir tema</h2>
 
-		<a href="/JEE_ECP/dispatcher/home">Volver a home</a>
+	<form action="/JEE_ECP/dispatcher/anadirtema" method="POST">
+		<label for="nombre">Nombre del tema: </label>
+		<input id="nombre" name="nombre" type="text"/><br/>
+		<label for="pregunta">Pregunta del tema: </label>
+		<input id="pregunta" name="pregunta" type="text"/><br/><br/>
+		<input type=submit value="A&ntilde;adir tema"/>
+	</form>
+	
+	<br/><br/>
+	
+	<a href="/JEE_ECP/dispatcher/home">Volver a home</a>
 </body>
 </html>
